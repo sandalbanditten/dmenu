769c561f5ade7bac041c3a1531f6f8a306f4ce3e
sandalbanditten <sandalbanditten@tutanota.com>
2023-02-27 21:18:34 +0100

    Updated to dmenu 0.5.2

--- config.h
+++ config.h
@@ -1,25 +1,24 @@
-
+/* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Fira Code:size=8"
 };
-static const char *prompt  = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                            fg         bg       */
 	[SchemeNorm]          = { "#ebdbb2", "#282828" },
-	[SchemeSel]           = { "#282828", "#ebdbb2" },
+	[SchemeSel]           = { "#282828", "#928374" },
 	[SchemeNormHighlight] = { "#b8bb26", "#282828" },
-	[SchemeSelHighlight]  = { "#98971a", "#ebdbb2" },
+	[SchemeSelHighlight]  = { "#98971a", "#928374" },
 	[SchemeOut]           = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-/* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
-static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -28,4 +27,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 4;
+static unsigned int border_width = 4;
